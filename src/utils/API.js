1b65cd311025eb9d46525151fecad870ae99adaa
taylorgonz <hello@taylorgonz.com>
2021-05-03 21:51:03 -0400

    homepagechange

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,5 +3,8 @@ import axios from "axios";
 export default {
     getEmployees: function() {
         return axios.get("https://randomuser.me/api/?results=100&nat=us&inc=name,email,picture,cell");
+    },
+    getEmployeeName: function() {
+        return axios.get("https://randomuser.me/api/?results=100&nat=us&inc=name,email,picture,cell");
     }
 }
